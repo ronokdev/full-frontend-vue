011ba17765d18ab22ce11fb0b0553aa0f4e71561
ronok <fuad.ronok@gmail.com>
2018-03-23 02:03:37 +0600

    Initial Commit

---
+++ src/components/testComponent.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+          <h1>
+                This is a TEST COMPONENT.VUE file
+          </h1>
+    </div>
+
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import test from '@/components/testComponent'
+
+Vue.use(Router)
+
+export default new Router({
+   mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/test/ronoktest',
+      component: test
+     }
+  ]
+})
